17e5ee986c756cf6f9dd15e986397f3c3c17080b
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-12 21:39:56 -0600

    6-9 completed

--- src/HellWorld.java
+++ src/HellWorld.java
@@ -17,21 +17,24 @@ public class HellWorld {
             //-it created an error
 
         // 4. Declare a long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
-        long myNumber;
-        System.out.println(3.14);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
         //-it creates an error saying that it cant be used before assignment
 
         // 5. Change your code to assign the value 3.14 to myNumber. What do you notice?
-            //-the error goes away
+            //-type error
 
         // 6. Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
-        //
+            //-it works...
+
         // 7. Change your code to assign the value 123 to myNumber.
-        //
+
         // 8. Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
-        //
+            //because 3.14 is a float and long requires an int
+
         // 9. Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
-        //
+            // -it errors, add f at the end or change it to a double
+
         // 10. Copy and paste the following code blocks one at a time and execute them
 
 
