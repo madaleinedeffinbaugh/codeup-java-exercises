d2ab6c9e43cd4361ab09d68749cc8b942d411002
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-12 21:29:28 -0600

    #1 completed

--- src/HellWorld.java
+++ src/HellWorld.java
@@ -1,5 +1,34 @@
 public class HellWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        //Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+        //
+        //Change your code to assign a character value to myString. What do you notice?
+        //
+        //Change your code to assign the value 3.14159 to myString. What happens?
+        //
+        //Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+        //
+        //Change your code to assign the value 3.14 to myNumber. What do you notice?
+        //
+        //Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+        //
+        //Change your code to assign the value 123 to myNumber.
+        //
+        //Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+        //
+        //Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+        //
+        //Copy and paste the following code blocks one at a time and execute them
+
+
+
+
+
     }
 }
