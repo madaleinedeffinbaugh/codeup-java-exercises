de16786ab750e367c7e08dd9ea051fc48b120a34
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-13 18:10:33 -0600

    console io exercise 2.1 completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f.", pi);
+        System.out.printf("The value of pi is approximately %.2f.%n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer :)");
+        try {
+            int usersInt = scanner.nextInt();
+            System.out.println(usersInt);
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Value entered was not an integer.");
+        }
+        catch(Exception e) {
+            System.out.println("Something went wrong.");
+        }
+
     }
 }
