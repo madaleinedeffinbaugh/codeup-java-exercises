27f1734ed34ac24ad5911b4f370803faaba5485f
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-13 18:23:11 -0600

    console io 2.2

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,13 +7,23 @@ public class ConsoleExercises {
         System.out.printf("The value of pi is approximately %.2f.%n", pi);
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter an integer :)");
+
         try {
+            //getting int from user
+            System.out.println("Please enter an integer :)");
             int usersInt = scanner.nextInt();
-            System.out.println(usersInt);
+            System.out.printf("Your integer: %d", usersInt);
+
+            //getting three words from user
+            System.out.println("Please enter three different words");
+            String wordOne = scanner.next();
+            String wordTwo = scanner.next();
+            String wordThree = scanner.next();
+            System.out.printf("one- %s, two- %s, three- %s", wordOne, wordTwo, wordThree);
+
         }
         catch(InputMismatchException e) {
-            System.out.println("Value entered was not an integer.");
+            System.out.println("Value entered was not of type specified.");
         }
         catch(Exception e) {
             System.out.println("Something went wrong.");
