e555844b70ad08b394610485bcfbae00b7fbfbc5
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-15 13:47:48 -0600

    1.2 completed

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,5 +2,8 @@ public class StringExercise {
     public static void main(String[] args) {
         String outputOne = "We don't need no education\nWe don't need no thought control";
         System.out.println(outputOne);
+
+        String outputTwo = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(outputTwo);
     }
 }
