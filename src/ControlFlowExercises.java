53d78a68fc48147592f0a599ac00e21827b0227e
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-14 20:55:32 -0600

    1.b complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,19 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
         //Loop Basics
-        // A. While
-        int i  = 5;
-        while (i <= 15) {
-            System.out.print(i + " ");
-            i++;
-        }
+
+        // a. While
+//        int i  = 5;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+
+        //b. Do While
+        long i = 2;
+        do {
+            System.out.println(i);
+            i = i * i;
+        } while(i <= 1000000);
     }
 }
