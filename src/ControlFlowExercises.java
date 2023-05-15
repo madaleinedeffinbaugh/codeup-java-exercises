bff6e3a42cd120a2c4061d9068c29d5307132975
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-14 21:00:13 -0600

    1.c completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -10,10 +10,32 @@ public class ControlFlowExercises {
 //        }
 
         //b. Do While
-        long i = 2;
-        do {
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i-=5;
+//        } while (i >= -10);
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i = i * i;
+//        } while(i <= 1000000);
+
+        // c. For
+        //a re-factored
+//        for(int i = 5; i <=15; i++) {
+//            System.out.println(i + " ");
+//        }
+
+        // first do-while on b re-factored
+//        for(int i = 100; i >= -10; i -=5) {
+//            System.out.println(i);
+//        }
+
+        // second do-while on b re-factored
+        for(long i = 2; i <= 1000000; i *= i) {
             System.out.println(i);
-            i = i * i;
-        } while(i <= 1000000);
+        }
     }
 }
