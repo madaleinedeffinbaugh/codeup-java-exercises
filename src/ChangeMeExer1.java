5886731038ec3655c17c9880a3bd655977f44761
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-17 19:44:01 -0600

    creating instance/testing methods

---
+++ src/ChangeMeExer1.java
@@ -0,0 +1,12 @@
+public class ChangeMeExer1 {
+
+    public static void main(String[] args) {
+        //creating instance
+        Person myPerson = new Person();
+
+        //testing methods
+        myPerson.setName("Maddie");
+        System.out.println(myPerson.getName());
+        myPerson.sayHello();
+    }
+}
