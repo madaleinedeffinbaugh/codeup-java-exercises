87417e31627a54276185e21a75ddce6ff80a49fc
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-18 20:10:09 -0600

    created util package, added bare bones input class

---
+++ src/util/Input.java
@@ -0,0 +1,30 @@
+package util;
+import java.util.Scanner;
+
+public class Input {
+
+    private Scanner scanner;
+
+    Input() {
+        scanner = new Scanner(System.in);
+    }
+
+    String getString() {
+
+    }
+    boolean yesNo() {
+
+    }
+    int getInt(int min, int max) {
+
+    }
+    int getInt() {
+
+    }
+    double getDouble(double min, double max) {
+
+    }
+    double getDouble() {
+
+    }
+}
