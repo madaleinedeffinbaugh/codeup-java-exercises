be0f18a2cab96960e7e6406abe433a8c296f3e07
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-18 20:16:09 -0600

    added meat to methods

--- src/util/Input.java
+++ src/util/Input.java
@@ -10,21 +10,32 @@ public class Input {
     }
 
     String getString() {
-
+        return scanner.nextLine();
     }
     boolean yesNo() {
-
+        String userIn = scanner.nextLine();
+        return userIn.equalsIgnoreCase("y") || userIn.equalsIgnoreCase("yes");
     }
     int getInt(int min, int max) {
-
+        int userIn = Integer.parseInt(scanner.nextLine());
+        while (userIn < min || userIn > max) {
+            System.out.println("Number was not in range, try again:");
+            userIn = Integer.parseInt(scanner.nextLine());
+        }
+        return userIn;
     }
     int getInt() {
-
+        return Integer.parseInt(scanner.nextLine());
     }
     double getDouble(double min, double max) {
-
+        int userIn = Integer.parseInt(scanner.nextLine());
+        while (userIn < min || userIn > max) {
+            System.out.println("Number was not in range, try again:");
+            userIn = Integer.parseInt(scanner.nextLine());
+        }
+        return userIn;
     }
     double getDouble() {
-
+        return Double.parseDouble(scanner.nextLine());
     }
 }
