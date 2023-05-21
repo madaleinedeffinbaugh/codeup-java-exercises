ae77daa3990f2970fffbf9aee1ea2f9f0fe0a71d
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-21 17:32:53 -0600

    added method to count circles

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,14 +2,20 @@ package shapes;
 public class Circle {
 
     private double radius;
+    private static int circles;
 
     public Circle(double radius) {
         this.radius = radius;
+        addCircle();
     }
     public double getArea() {
         return Math.PI * (radius * radius);
     }
     public double getCircumference() {
         return 2 * Math.PI * radius;
+    }
+
+    public static void addCircle() {
+        circles += 1;
     }
 }
