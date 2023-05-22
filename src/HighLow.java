68f58dd96681096f6236884c5bebd4199b9de212
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-22 14:09:40 -0600

    refactored

--- src/HighLow.java
+++ src/HighLow.java
@@ -2,63 +2,73 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HighLow {
-    public static void main(String[] args) {
-        Random rand = new Random();
-        Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
         String userConfirm;
 
+        //I created the range 1-100 and the maxGuesses as parameters/arguments so that they can be changed easily.
+        // Maybe a game where it also randomly generates the range....
         do {
-            int randomNumber = rand.nextInt(100) + 1;
-            int userGuess = 0;
-            int numOfGuesses = 0;
-            do {
-                do {
-                    try {
-                        System.out.println("\n\nPlease guess a number between 1 and 100:");
-                        userGuess = Integer.parseInt(scanner.nextLine());
-                    } catch (NumberFormatException e) {
-                        System.out.println("That is not a number, try again.");
-                    }
-                } while(userGuess < 1 || userGuess > 100);
-
-                numOfGuesses++;
-
-                if(userGuess == randomNumber) {
-                    System.out.println("GOOD GUESS!");
-                    System.out.println("YOU WIN!");
-                } else if (userGuess > randomNumber) {
-                    if(numOfGuesses == 10) {
-                        System.out.println("You are too high, and out of guesses! You lose");
-                        break;
-                    } else if(numOfGuesses == 9) {
-                        System.out.println("LOWER");
-                        System.out.printf("You have %d guess remaining.", 10-numOfGuesses);
-                    }
-                    else {
-                        System.out.println("LOWER");
-                        System.out.printf("You have %d guesses remaining.", 10-numOfGuesses);
-                    }
-
-                } else {
-                    if(numOfGuesses == 10) {
-                        System.out.println("You are too low, and out of guesses! You lose");
-                        break;
-                    } else if(numOfGuesses == 9) {
-                        System.out.println("HIGHER");
-                        System.out.printf("You have %d guess remaining.", 10-numOfGuesses);
-                    } else {
-                        System.out.println("HIGHER");
-                        System.out.printf("You have %d guesses remaining.", 10-numOfGuesses);
-                    }
-                }
-
-            } while(userGuess != randomNumber);
-
+            gamePlay(1, 100, 10);
             System.out.println("Press Q to quit. Enter anything else to play again.");
             userConfirm = scanner.nextLine();
-        } while(!userConfirm.equalsIgnoreCase("q"));
+        } while (!userConfirm.equalsIgnoreCase("q"));
 
+    }
+
+    public static void gamePlay(int bottomRange, int topRange, int maxGuess) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(topRange - (bottomRange - 1)) + bottomRange;
+        System.out.println("CHEAT NUMBER = " + randomNumber);
+        int numOfGuesses = 0;
+        while (true) {
+            int userGuess = getGuess(bottomRange, topRange);
+            numOfGuesses++;
+            if (checkGuess(randomNumber, userGuess) || numOfGuesses >= maxGuess) {
+                break;
+            }
+            displayGuessesLeft(numOfGuesses, maxGuess);
+        }
+    }
+
+    public static int getGuess(int bottomRange, int topRange) {
+        int userGuess = 0;
+        try {
+            System.out.println("\n\nPlease guess a number between " + bottomRange + " and " + topRange + ":");
+            userGuess = Integer.parseInt(scanner.nextLine());
+            if (userGuess < bottomRange || userGuess > topRange) {
+                System.out.println("Number not in range, try again.");
+                return getGuess(bottomRange, topRange);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("That is not a number, try again.");
+        }
+
+        return userGuess;
+    }
+
+    public static boolean checkGuess(int correctAnswer, int userGuess) {
+        if (userGuess == correctAnswer) {
+            System.out.println("GOOD GUESS!");
+            System.out.println("YOU WIN!");
+            return true;
+        } else if (userGuess > correctAnswer) {
+            System.out.println("TOO HIGH");
+        } else {
+            System.out.println("TOO LOW");
+        }
+        return false;
+    }
+
+    public static void displayGuessesLeft(int numOfGuesses, int maxGuesses) {
+        if (numOfGuesses < maxGuesses) {
+            System.out.println("You have " + (maxGuesses - numOfGuesses) + " guesses left.");
+        } else if (maxGuesses - numOfGuesses == 1) {
+            System.out.println("You have " + (maxGuesses - numOfGuesses) + " guess left.");
+        } else {
+            System.out.println("You are out of guesses! You lose!");
+        }
     }
 
 }
