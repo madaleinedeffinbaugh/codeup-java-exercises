7a43708a73f566b8a8e9a5992c173c312714435b
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-23 08:09:39 -0600

    changed where main method was

--- src/Person.java
+++ src/Person.java
@@ -18,4 +18,37 @@ public class Person {
         //TODO: print a message to the console using the person's name
         System.out.println("Hello " + this.name + "!");
     }
+
+
+    public static void main(String[] args) {
+        //creating instance
+        Person myPerson = new Person("NotMaddie");
+
+        //testing methods
+        System.out.println(myPerson.getName());
+        myPerson.setName("Maddie");
+        System.out.println(myPerson.getName());
+        myPerson.sayHello();
+
+        //running test code
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+    }
+
 }
