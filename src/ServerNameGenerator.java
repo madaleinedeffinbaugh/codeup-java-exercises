7d5d04eec85e101bfdd32983942e64c1cfd573a1
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-24 13:44:19 -0600

    arrays made

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,9 @@
+public class ServerNameGenerator {
+
+    public static String[] adj = {"cruel", "dangerous", "helpful", "jumpy", "petty", "plain", "cheerful", "brave", "amused", "nervous"};
+    public static String[] nouns = {"dog", "horse", "cow", "army", "snail", "doctor", "toothbrush", "mountain", "pest", "oatmeal"};
+
+    public static void main(String[] args) {
+
+    }
+}
