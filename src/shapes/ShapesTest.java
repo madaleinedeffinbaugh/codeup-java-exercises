50548e1ab713d6433764951b92ccdbcb95042d34
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-25 20:46:22 -0600

    tested sqaure class

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,5 +5,9 @@ public class ShapesTest {
         Rectangle box1 = new Rectangle(4, 5);
         System.out.println(box1.getPerimeter());
         System.out.println(box1.getArea());
+
+        Rectangle box2 = new Square(5);
+        System.out.println(box2.getPerimeter());
+        System.out.println(box2.getArea());
     }
 }
