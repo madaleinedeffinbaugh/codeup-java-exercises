f11ef88e0bd7d94fd83674fc6f5b5a2fb95cf94c
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-25 20:55:18 -0600

    exercise complete

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,7 +2,22 @@ package shapes;
 
 public class Square extends Rectangle {
 
+    private double side;
+
     public Square(double side) {
         super(side, side);
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+//        System.out.println("This is the override method");
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+//        System.out.println("This is the override method");
+        return 4 * side;
     }
 }
